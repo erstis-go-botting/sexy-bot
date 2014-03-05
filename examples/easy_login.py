71c74fb68378b1097130d556a84af35b9b717d40
alskgj <dimitri.wessels@gmail.com>
2014-03-05 12:57:01 +0100

    Create easy_login.py

---
+++ examples/easy_login.py
@@ -0,0 +1,24 @@
+import requests
+
+data = dict()
+
+login_url = "http://en.ogame.gameforge.com/main/login"
+
+data["kid"] = ""
+data["uni"] = "s124-en.ogame.gameforge.com"
+data["login"] = "alskgj"
+data["pass"] = "nid12345"
+
+r = requests.post(login_url, data)
+
+print(r.text) # gets the whole sourcecode and prints it
+
+
+###############################################
+# CHEAT SHEET
+#
+# post: requests.post
+# get: requests.get
+# sourcecode: requests.get.text
+#
+###############################################
