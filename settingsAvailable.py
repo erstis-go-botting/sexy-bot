1d7646b4aea12f1257464a0dc9f356150f1accfa
rollator <lwolf@student.ethz.ch>
2014-03-10 00:16:05 +0100

    added some output and the possibility to use universe 82

--- settingsAvailable.py
+++ settingsAvailable.py
@@ -1,9 +1,14 @@
 import os
 
 #checks if settings.ini should be generated. if not given universe, username and password it will generate a settings.ini with the default account
+#This settings_generator will only work for universe 82 if the flag argument is given als True(to make sure that universe 82 is intended)
 
-
-def settings_generator(universe = 82, username = 'defaultName', password = 'defaultPassword'):
+def settings_generator(universe = 82, username = 'defaultName', password = 'defaultPassword', flag=False):
+    if (universe == 82 and not(flag)) or (username == 'defaultName') or (password == 'defaultPassword'):
+        print("Not all fields specified, fallback on default configuration")
+        universe = 82
+        username = 'defaultName'
+        password = 'defaultPassword'
     if not (os.path.isdir('settings')):
         os.makedir('settings')
     path = os.path.normcase('settings/settings.ini')
